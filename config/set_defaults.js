90e592b056ed6182a24d953b070a40d79b758ba9
Mark Birbeck <mark.birbeck@gmail.com>
2013-08-09 17:08:36 +0100

    Set module default config values.

---
+++ config/set_defaults.js
@@ -0,0 +1,63 @@
+/**
+ * Set the default configuration values:
+ */
+
+var CONFIG = require('config');
+
+CONFIG.setModuleDefaults('amqp', {
+  publish: {
+
+  /**
+   * The mumber of messages that should be accumulated into a batch
+   * before publishing the whole batch:
+   */
+
+    batchSize: 500
+  }
+, subscribe: {
+
+    /**
+     * How often (in minutes) to check for messages in subscribe():
+     */
+
+    pollInterval: 5
+
+  , rateLimit: {
+
+      /**
+       * When true, don't rate limit but send messages immediately:
+       */
+
+      fireImmediately: false
+
+      /**
+       * When rate limiting, the number of items per unit of time that should
+       * be passed through:
+       */
+
+    , tokensPerInterval: 10
+
+      /**
+       * When rate limiting, the unit of time that the tokensPerInterval value
+       * will operate over:
+       */
+
+    , interval: 'seconds'
+    }
+  }
+});
+
+CONFIG.setModuleDefaults('aws', {
+
+  /**
+   * Region, which must be one of:
+   *
+   *  us-east-1, us-west-1, us-west-2, eu-west-1, ap-southeast-1, ap-northeast-1
+   */
+
+  region: 'eu-west-1'
+
+, sqs: {
+    visibilityTimeout: 120
+  }
+});
